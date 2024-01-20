cbe8645ea715d766ebc1e405b65823b027b9d844
LakshmiNarayanaKanamarlapudi <kanamala@mail.uc.edu>
2024-01-20 17:37:35 -0500

    lab1

---
+++ labs/lab1/index.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+
+	printf("Content-Type: text/html\n\n");
+	printf("<!DOCTYPE html>\n");
+	printf("<html>\n");
+	printf("<head>\n");
+	printf("<title>CGI in Web Application</title>\n");
+	printf("</head>\n");
+	printf("<body>\n");
+	printf("<h1>COURSE : Web Application Programming and Hacking</h1>\n");
+	printf("<p> Instructor : Dr. Phu Phung </p>\n");
+	printf("<p> Student : Lakshmi Narayana Kanamarlapudi</p>\n");
+	printf("</body>\n");
+	printf("</html>\n");
+	return 0;
+}

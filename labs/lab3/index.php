d52aeb41e9163dd07a203118b7df188b79905821
kanamala <LakshmiNarayanaKanamarlapudi>
2024-02-27 18:38:16 -0500

    lab3

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();    
-	if (checklogin_mysql($_POST["username"],$_POST["password"])) {
+	 if (checklogin_mysql($_POST["username"],$_POST["password"])) {
 ?>
 	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
 <?php		
@@ -8,25 +8,24 @@
 		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
 		die();
 	}
-	function checklogin($username, $password) {
-		$account = array("admin","1234");
-		if (($username== $account[0]) and ($password == $account[1])) 
-		  return TRUE;
-		else 
-		  return FALSE;
-  	}
+	
   	function checklogin_mysql($username, $password) {
-  		$mysqli = new mysqli('localhost','kanamala','waph@UC!2024','waph');
-  		if($mysqli->connect_errno){
-  			printf("Database connection failed: %s\n", $mysqli->connect_errno);
-  			exit();
-  		}
-  		$sql = "SELECT * FROM users WHERE username='" . $username . "' ";
-  		$sql = $sql . " AND password = md5('" . $password . "')";
-  		//echo "DEBUG>sql= $sql"; //return TRUE
-  		$result = $mysqli->query($sql);
-  		if($result->num_rows ==1)
-  			return TRUE;
-  		return FALSE;
+		$mysqli= new mysqli('localhost',
+							'kanamala',
+							'waph@UC!2024',
+							'waph');
+		if ($mysqli->connect_errno){
+			printf("DATABASE COONECTION FAILED: %s\n", $mysqli->connect_error);
+			exit();
+		} 
+		 $prepared_sql ="SELECT * FROM users WHERE username= ? AND password = md5(?);";
+		 $stmt = $mysqli->prepare($prepared_sql);
+		 $stmt-> bind_param("ss",$username,$password);
+		 $stmt->execute();
+		  // echo "DEBUG>sql= $sql";
+		  $result = $stmt->get_result();
+		  if($result->num_rows ==1)
+		  	return TRUE;
+		  return FALSE;
   	}
 ?>

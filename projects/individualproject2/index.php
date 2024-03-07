2e1ade3bfc1cddc17491d3e49a74babc05adf932
kanamala <LakshmiNarayanaKanamarlapudi>
2024-03-07 11:59:31 -0500

    lec17

--- projects/individualproject2/index.php
+++ projects/individualproject2/index.php
@@ -1,9 +1,11 @@
 <?php
-	session_start();    
-	 if(isset($_POST["username"]) and isset($_POST["password"])) {
+	session_set_cookie_params(15*60,"/","kanamala.waph.io",TRUE,TRUE);
+	session_start();
+	if(isset($_POST["username"]) and isset($_POST["password"])) {
 		 if (checklogin_mysql($_POST["username"],$_POST["password"])) {
 		 	$_SESSION['authenticated'] = TRUE;
-		 	$_SESSION['username'] = $_POST["username"];	
+		 	$_SESSION['username'] = $_POST["username"];
+		 	$_SESSION["browser"] = $_SERVER['HTTP_USER_AGENT'];	
 		}else{
 			session_destroy();
 			echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
@@ -14,6 +16,12 @@
 	if (!isset($_SESSION['authenticated']) or $_SESSION['authenticated']!= TRUE) {
 		session_destroy();
 		echo "<script>alert('You have not login. Please login first')</script>";
+		header("Refresh: 0; url=form.php");
+		die();
+	}
+	if ( $_SESSION['browser'] != $_SERVER['HTTP_USER_AGENT']){
+		session_destroy();
+		echo "<script>alert('Session hijacking attack is detected!');</script>";
 		header("Refresh: 0; url=form.php");
 		die();
 	}
